9676b9ba279bdf47493682e80447db4c0b53840c
Kai Du <alanduu50@gmail.com>
2016-06-04 16:59:49 +0100

    canMove fix.

--- web/game/tutorial1.php
+++ web/game/tutorial1.php
@@ -1,6 +1,9 @@
 <html>
   <head>
     <script src="https://code.createjs.com/createjs-2015.11.26.min.js"></script>
+    <script>
+    lightbox_alert("Hi", "Hello world!");
+    </script>
     <style>
         body { 
           position: relative;
@@ -22,22 +25,26 @@
           left: 0;
           z-index: 0;
         }*/
-        
       </style>
-  </head>
-  <body>
-      
+
   <script src="https://code.createjs.com/createjs-2015.11.26.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+  <script src="../interface/js/lightbox.js"></script>
+  <script src="../interface/js/common.js"></script>
+  <link rel="stylesheet" href="css/lightbox.css">
   <style type="text/css">
-	  @font-face {
-		  font-family: 04b_19;
-		  src:url(./fonts/04B_19__.TTF);
-	  }
+    @font-face {
+      font-family: 04b_19;
+      src:url(./fonts/04B_19__.TTF);
+    }
   </style>
+  
+  </head>
+  <body>
+  <?php include 'includes/lightbox.php'; ?>
+      
   <canvas id="gameCanvas" width="5000px" height="5000px"></canvas>
   <script src="tutorial1.js"></script>
-  <script src="../interface/js/common.js"></script>
   </body>
 </html>
 
