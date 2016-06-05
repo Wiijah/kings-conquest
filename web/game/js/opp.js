ac9f560317365852ffcd465c8635880600b3384a
Kai Du <alanduu50@gmail.com>
2016-06-05 15:15:02 +0100

    Debugging opp

--- web/game/js/opp.js
+++ web/game/js/opp.js
@@ -2,7 +2,7 @@ var opp_id = "0"; //TODO: remove this line
 
 function getOpp() {
   console.log("getOpp Start with Opp_ID: " + opp_id);
-  comPost("ajax/opp", {"opp_id" : String(opp_id)}, getOpp, function(data) {
+  $.comPost("ajax/opp", {"opp_id" : String(opp_id)}, getOpp, function(data) {
     console.log("Opponent data !" + data.opp_id);
     if (data.error_code != 0) {
       console.log("BAD BAD BAD BAD BAD opp Return !");
