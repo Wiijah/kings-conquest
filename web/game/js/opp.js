e5b5c1141d42f5cfd50f494b8d2d548fe697b9e8
Kai Du <alanduu50@gmail.com>
2016-06-05 15:29:09 +0100

    Opp.js

--- web/game/js/opp.js
+++ web/game/js/opp.js
@@ -3,17 +3,15 @@ var opp_id = "0"; //TODO: remove this line
 function getOpp() {
   console.log("getOpp Start with Opp_ID: " + opp_id);
 
-  $(document).ready(function() {
-    $.comPost("ajax/opp", {"opp_id" : String(opp_id)}, getOpp, function(data, status) {
-      console.log("Opponent data !" + data.opp_id);
-      if (data.error_code != 0) {
-        console.log("BAD BAD BAD BAD BAD opp Return !");
-        return;
-      }
-      opp_id = data.opp_id;
-      handleOpponent(data.json);
-      console.log("getOpp Return !");
-      getOpp();
-    });
+  $.comPost("ajax/opp", {"opp_id" : String(opp_id)}, getOpp, function(data, status) {
+    console.log("Opponent data !" + data.opp_id);
+    if (data.error_code != 0) {
+      console.log("BAD BAD BAD BAD BAD opp Return !");
+      return;
+    }
+    opp_id = data.opp_id;
+    handleOpponent(data.json);
+    console.log("getOpp Return !");
+    getOpp();
   });
 }
