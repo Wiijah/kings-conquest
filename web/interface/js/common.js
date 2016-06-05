a08008639f2e9b8bdd4d0110d2dd9a33a1a941d5
Kai Du <alanduu50@gmail.com>
2016-06-05 17:28:07 +0100

    GetOPP

--- web/interface/js/common.js
+++ web/interface/js/common.js
@@ -112,10 +112,6 @@ function handle_ajax_error(jqXHR, textStatus, errorThrown) {
 
 function handleComError(jqXHR, textStatus, errorThrown) {
   getOpp();
-  console.log("comError!" + jqXHR +textStatus+errorThrown);
-  console.debug(jqXHR);
-  console.debug(textStatus);
-  console.debug(errorThrown);
    //lightbox_alert("Server Error", "There was an error connecting to the server. Please try again later.");
 }
 /* Function that checks if a string's length is between two numbers. */
