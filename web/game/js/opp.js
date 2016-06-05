69c85cf5d44aa53174ad00a890a0f0350d5bec96
Kai Du <alanduu50@gmail.com>
2016-06-05 15:15:36 +0100

    dollar comPost insteado f compOst

--- web/game/js/opp.js
+++ web/game/js/opp.js
@@ -2,15 +2,18 @@ var opp_id = "0"; //TODO: remove this line
 
 function getOpp() {
   console.log("getOpp Start with Opp_ID: " + opp_id);
-  $.comPost("ajax/opp", {"opp_id" : String(opp_id)}, getOpp, function(data) {
-    console.log("Opponent data !" + data.opp_id);
-    if (data.error_code != 0) {
-      console.log("BAD BAD BAD BAD BAD opp Return !");
-      return;
-    }
-    opp_id = data.opp_id;
-    handleOpponent(data.json);
-    console.log("getOpp Return !");
-    getOpp();
+
+  $(document).ready(function() {
+    $.comPost("ajax/opp", {"opp_id" : String(opp_id)}, getOpp, function(data) {
+      console.log("Opponent data !" + data.opp_id);
+      if (data.error_code != 0) {
+        console.log("BAD BAD BAD BAD BAD opp Return !");
+        return;
+      }
+      opp_id = data.opp_id;
+      handleOpponent(data.json);
+      console.log("getOpp Return !");
+      getOpp();
+    });
   });
 }
