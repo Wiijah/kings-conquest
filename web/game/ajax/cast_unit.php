755d0fa9a459b3669e91130846edd3b6383b6a05
Kai Du <alanduu50@gmail.com>
2016-06-08 13:30:25 +0100

    Wizard skill backend

--- web/game/ajax/cast_unit.php
+++ web/game/ajax/cast_unit.php
@@ -9,6 +9,12 @@ $y = secureStr($_POST['y']);
 // get player team
 $team = $TEAM_COLOURS[$player->colour];
 
+$AOE = "(x = '{$x}' AND y = '{$y}')
+           OR (x = '".($x - 1)."' AND y = '{$y}')
+           OR (x = '".($x + 1)."' AND y = '{$y}')
+           OR (x = '{$x}' AND y = '{$y + 1}')
+           OR (x = '{$x}' AND y = '".($y - 1)."')";
+
 //get both units
 $result = $db->query("SELECT * FROM units JOIN classes ON units.class_id = classes.class_id WHERE unit_id = '{$caster_id}' AND room_id = '{$room_id}'");
 $caster = $result->fetch_object();
@@ -36,13 +42,10 @@ if ($caster->skill == "Shield") {
          jsonPair("buff_id", 2) 
     .",".jsonPair("unit_id", $fetch->unit_id));
   }
-} else if ($caster->skill == "Battle Cry") {
-  $result = $db->query("SELECT * FROM units WHERE room_id = '{$room_id}' AND team != '{$team}'");
+} else if ($caster->skill == "Magic Damage") {
+  $result = $db->query("SELECT * FROM units WHERE room_id = '{$room_id}' AND team != '{$team}' AND {$AOE}");
   while ($fetch = $result->fetch_object()) {
-    $db->query("INSERT INTO buff_instances (buff_id, unit_id, turns_left) VALUES ('2', '{$fetch->unit_id}', '6')");
-    $actions[] = action("apply_buff",
-         jsonPair("buff_id", 2) 
-    .",".jsonPair("unit_id", $fetch->unit_id));
+    $actions[] = attack_unit($caster, $fetch);
   }
 }
 
