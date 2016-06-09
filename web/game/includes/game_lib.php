3d79cea83648861c8e515a738d21cf9b0fbb4e60
Kai Du <alanduu50@gmail.com>
2016-06-09 16:26:48 +0100

    archer buff server side debuff -20% atk

--- web/game/includes/game_lib.php
+++ web/game/includes/game_lib.php
@@ -162,7 +162,11 @@ function attack_unit($attacker, $target) {
 
     $result = $db->query("SELECT * FROM buff_instances WHERE unit_id = '{$attacker->unit_id}' AND buff_id = 2");
     $battle_cry = $result->fetch_object();
-    $damage = ceil($damage * 1.2);
+    if ($battle_cry) $damage = ceil($damage * 1.2);
+
+    $result = $db->query("SELECT * FROM buff_instances WHERE unit_id = '{$attacker->unit_id}' AND buff_id = 3");
+    $exhaust = $result->fetch_object();
+    if ($exhaust) = ceil($damage * 0.8);
 
     $crit = rand(1,100) <= ($attacker->luck * 100) ? '1' : '0';
     if ($crit == '1') $damage *= 2;
