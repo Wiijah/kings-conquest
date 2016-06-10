95eada9706f59ffb0ab3e561ce982135d288e22a
Kai Du <alanduu50@gmail.com>
2016-06-10 11:02:59 +0100

    ACH FIX

--- web/game/includes/game_lib.php
+++ web/game/includes/game_lib.php
@@ -297,9 +297,10 @@ function gameEnd($winner, $loser, $reason) {
 
   /* Give achievements if appropriate */
   if ($winner->wins == 0) giveAch("first_win", $winner, $room_id);
+  if ($loser->losses == 0) giveAch("first_lost", $loser, $room_id);
   if ($winner->wins == 49) giveAch("50_wins", $winner, $room_id);
   if ($winner->wins == 99) giveAch("100_wins", $winner, $room_id);
-  if ($loser_part->unit_kills == 0) giveAch("perfect_win", $loser, $room_id);
+  if ($loser_part->unit_kills == 0) giveAch("perfect_win", $winner, $room_id);
 
   return $actions;
 }
